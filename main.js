93f7a1a66a65924db3ceac1514143a7591136a47
Alan Turing <trond@blacktie-llc.com>
2019-08-23 16:24:47 -0600

    Fixed name input issue

--- main.js
+++ main.js
@@ -9,12 +9,14 @@ var maxNum = document.querySelector('.max-number');
 var rangeButton = document.querySelector('.range-button');
 var submitGuessButton = document.querySelector('#submit-guess-button');
 var randomNumber = null;
-var challengerOneName = document.querySelector('.challenger-1-name');
-var challengerTwoName = document.querySelector('.challenger-2-name');
 var challengerOneNew = document.querySelector('.challenger-1-name-change');
 var challengerTwoNew = document.querySelector('.challenger-2-name-change');
-var challengerOneNameInput = document.querySelector('.AAA');
-var challengerTwoNameInput = document.querySelector('.AAAA');
+var latestScoreNameOne = document.querySelector('.latest-score-name-1');
+var latestScoreNameTwo = document.querySelector('.latest-score-name-2');
+var challengerOneNameInput = document.querySelector('.player-1-input-name');
+var challengerTwoNameInput = document.querySelector('.player-2-input-name');
+// var styleButton = document.querySelector(".style-button");
+// var changeColorButton = document.querySelector('.change-color-button');
 
 rangeButton.addEventListener('click', updateRange);
 rangeButton.addEventListener('click', createRandomNum);
@@ -39,8 +41,13 @@ function updateCurrentGuess() {
 };
 
 function enterName(){
-  console.log(challengerOneNew.innerText)
   challengerOneNew.innerText = challengerOneNameInput.value;
-  console.log(challengerTwoNew.innerText)
   challengerTwoNew.innerText = challengerTwoNameInput.value;
+  latestScoreNameOne.innerText = challengerOneNameInput.value;
+  latestScoreNameTwo.innerText = challengerTwoNameInput.value;
 };
+
+// //changeColorButton.addEventListener('click', funtion () {
+//   styleButton.style.backgroundColor = '';
+//
+// });
